3d8638d3b4ec881c11c6810b80fb7a7f746f4cc3
nikrstic <nikola.krstic2@pmf.edu.rs>
2024-02-03 23:38:56 +0100

    added max aritmetic

--- procedure/zad12.py
+++ procedure/zad12.py
@@ -1,1 +1,8 @@
-L=[1,2,3,4,5,6,4]
+L=[41,32,53,34,25,16,54]
+max=(L[0]+L[1])/2
+def aritmeticka(a,b):
+    return (a+b)/2
+for i in range(0,len(L)-1):
+    if aritmeticka(L[i],L[i+1]) > max:
+        max = aritmeticka(L[i],L[i+1])
+print(max)
