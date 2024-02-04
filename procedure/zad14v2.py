54c6fb9edfe040aa4f1885575d9efd19361a8e28
nikrstic <nikola.krstic2@pmf.edu.rs>
2024-02-04 12:52:34 +0100

    ispisuje nedelju

--- procedure/zad14v2.py
+++ procedure/zad14v2.py
@@ -1,5 +1,5 @@
 L1=["adam","pera","branko","laza","mika","aleksa"]
 L=[6,4,6,9,10,6]
 parovi=list(zip(L,L1))
-parovi.sort()
+parovi.sort(reverse=True)
 print(parovi)
--- strukture/zad03.py
+++ strukture/zad03.py
@@ -1,8 +1,9 @@
 from datetime import datetime,timedelta
 sada=datetime.now()
 print(sada)
-print(sada.isoweekday())
+
 dani=("ponedeljak", "utorak","sreda","cetvrtak","petak","subota","nedelja")
+print(dani[sada.isoweekday()])
 for i in range (1,7):
     sada+=timedelta(days=1)
     print(dani[sada.isoweekday()-1])
