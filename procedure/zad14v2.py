5a07cf1c84db1ea2e50bf9f6a500ac6094376c1a
nikrstic <nikola.krstic2@pmf.edu.rs>
2024-02-04 13:07:17 +0100

    popravljen14

--- procedure/zad14v2.py
+++ procedure/zad14v2.py
@@ -1,5 +1,5 @@
-L1=["adam","pera","branko","laza","mika","aleksa"]
-L=[6,4,6,9,10,6]
+L1=["adam","pera","branko","laza","mika","aleksa","asan"]
+L=[6,4,6,9,10,6,6]
 parovi=list(zip(L,L1))
-parovi.sort(key=lambda x:)
+parovi.sort(key=lambda x: ( -x[0], x[1]))
 print(parovi)
