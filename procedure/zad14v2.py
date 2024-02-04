cb48d6a61074e881c5af86e597a22dea91e85fd0
nikrstic <nikola.krstic2@pmf.edu.rs>
2024-02-04 12:55:24 +0100

    nedelja def resena

--- procedure/zad14v2.py
+++ procedure/zad14v2.py
@@ -1,5 +1,5 @@
 L1=["adam","pera","branko","laza","mika","aleksa"]
 L=[6,4,6,9,10,6]
 parovi=list(zip(L,L1))
-parovi.sort(reverse=True)
+parovi.sort(key=lambda x:)
 print(parovi)
--- strukture/zad03.py
+++ strukture/zad03.py
@@ -3,7 +3,7 @@ sada=datetime.now()
 print(sada)
 
 dani=("ponedeljak", "utorak","sreda","cetvrtak","petak","subota","nedelja")
-print(dani[sada.isoweekday()])
+print(dani[sada.isoweekday()-1])
 for i in range (1,7):
     sada+=timedelta(days=1)
     print(dani[sada.isoweekday()-1])
